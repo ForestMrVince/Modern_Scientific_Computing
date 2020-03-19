de2ac7220109bd55e3bafef0db177683e797daa7
ForestMrVince <37427388+ForestMrVince@users.noreply.github.com>
2020-03-19 19:05:20 +0800

    upload dir 4-9 including function lyapsym

---
+++ src/4-9/lyapsym.m
@@ -0,0 +1,20 @@
+function [X] = lyapsym(A, B, C)
+% lyapsym 求解Lyapunov方程或Sylvester方程的解析解
+%   Lyapunov方程形式: A * X + X * A.' = -C
+%   Sylvester方程形式: A * X + X * B = -C
+%   输入: 
+%       A: 如方程形式所示
+%       B: 如方程形式所示
+%       C: 如方程形式所示
+%   输出: 
+%       X: 如方程形式所示
+
+if nargin == 2, C = B; B = A'; end
+[nr, nc] = size(C);
+A0 = kron(A, eye(nc)) + kron(eye(nr), b.');
+try
+    C1 = C.'; x0 = -inv(A0) * C1(:); X = reshape(x0, nc, nr);
+catch
+    error('singular matrix found.');
+end
+
